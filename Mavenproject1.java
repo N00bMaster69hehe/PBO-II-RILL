7ff916febef66db5bd7b9bd99fe6a1223b07271c
Dennis Hansen Sutanto <163019059+N00bMaster69hehe@users.noreply.github.com>
2025-04-02 20:23:53 +0700

    Add files via upload

--- Mavenproject1.java
+++ Mavenproject1.java
@@ -1,22 +1,13 @@
-
 package latianmaven;
 import java.util.*;
 public class Mavenproject1 {
-    
     public static void main(String[] args) {
-        Set set = new HashSet();
-set.add("Sistem");
-set.add("Teknologi Informasi");
-set.add("Bisnis");
-set.add("Digital");
-set.add("Kewirausahaan");
-
-System.out.print("Elemen pada HashSet : ");
-System.out.println(set);
-
-Set sortSet = new TreeSet(set);
-System.out.print("Elemen pada TreeSet : ");
-System.out.println(sortSet);
-    }
+        String[] sentences = {"sistem", "informasi", "dan", "teknologi", "informasi"};
+Set<String> s = new HashSet<String>();
+for (String a : sentences)
+if (!s.add(a))
+System.out.println("Duplicate detected: " + a);
+System.out.println(s.size() + " distinct words: " + s);
+    } 
 }
 
